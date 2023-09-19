65aa6f4a6ec89cf5b3a0fd6f24e06e552e14c931
shixw-null <slava_volga@bk.ru>
2023-09-19 22:28:52 +0300

    Add modules for tables

---
+++ comments.py
@@ -0,0 +1,137 @@
+from database import connect_to_database
+import psycopg2
+
+# Функция для создания таблицы комментариев
+def create_comment_table():
+    try:
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            create_table_query = """
+            CREATE TABLE IF NOT EXISTS comments (
+                id serial PRIMARY KEY,
+                text TEXT,
+                user_id INTEGER REFERENCES users(id),
+                post_id INTEGER REFERENCES posts(id)
+            )
+            """
+            cursor.execute(create_table_query)
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return True
+    except psycopg2.Error as e:
+        print("Ошибка при создании таблицы комментариев:", e)
+
+# Функция для валидации данных комментария перед добавлением или обновлением
+def validate_comment_data(text):
+    if not text:
+        return False  # Если текст комментария пустой, возвращаем False
+    return True  # Если данные прошли все проверки, возвращаем True
+
+# Функция для добавления комментария
+def add_comment(text, user_id, post_id):
+    # Проверяем валидность данных перед добавлением
+    if not validate_comment_data(text):
+        return False  # Данные не прошли валидацию, не выполняем добавление
+
+    try:
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            insert_query = """
+            INSERT INTO comments (text, user_id, post_id)
+            VALUES (%s, %s, %s)
+            """
+            cursor.execute(insert_query, (text, user_id, post_id))
+            conn.commit()
+            new_comment = cursor.fetchone()  # Получаем данные о новом комментарии
+            cursor.close()
+            conn.close()
+            if new_comment:
+                return new_comment  # Возвращаем данные о комментарии
+            else:
+                return None  # Если данные о пользователе не найдены
+    except psycopg2.Error as e:
+        print("Ошибка при добавлении комментария:", e)
+        return False  # Ошибка при добавлении
+
+def delete_comment(comment_id):
+    try:
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            delete_query = "DELETE FROM comments WHERE id = %s"
+            cursor.execute(delete_query, (comment_id,))
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return True
+    except psycopg2.Error as e:
+        print("Ошибка при удалении комментария:", e)
+
+def get_all_comments():
+    try:
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            select_query = "SELECT * FROM comments"
+            cursor.execute(select_query)
+            comments = cursor.fetchall()
+            cursor.close()
+            conn.close()
+            return comments
+    except psycopg2.Error as e:
+        print("Ошибка при получении всех комментариев:", e)
+
+def get_comment_by_id(comment_id, fields=("id", "text")):
+    try:
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            select_query = "SELECT {} FROM comments WHERE id = %s".format(", ".join(fields))
+            cursor.execute(select_query, (comment_id,))
+            comment_data = cursor.fetchone()
+            cursor.close()
+            conn.close()
+            return comment_data
+    except psycopg2.Error as e:
+        print("Ошибка при получении комментария по ID:", e)
+
+def update_comment(comment_id, new_text):
+    # Проверяем валидность данных перед обновлением
+    if not validate_comment_data(new_text):
+        return False  # Данные не прошли валидацию, не выполняем обновление
+
+    try:
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            update_query = """
+            UPDATE comments
+            SET text = %s
+            WHERE id = %s
+            """
+            cursor.execute(update_query, (new_text, comment_id))
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return True
+    except psycopg2.Error as e:
+        print("Ошибка при обновлении информации о комментарями:", e)
+        return False  # Ошибка при обновлении
+        
+# Функция для получения всех комментариев к посту с сортировкой по дате создания (по умолчанию по возрастанию)
+def get_comments_for_post(post_id, order_by="created_at ASC"):
+    try:
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            select_query = f"SELECT * FROM comments WHERE post_id = %s ORDER BY {order_by}"
+            cursor.execute(select_query, (post_id,))
+            comments = cursor.fetchall()
+            cursor.close()
+            conn.close()
+            return comments
+    except psycopg2.Error as e:
+        print("Ошибка при получении комментариев:", e)
---
+++ posts.py
@@ -0,0 +1,148 @@
+from database import connect_to_database
+import psycopg2
+
+# Функция для создания таблицы постов
+def create_post_table():
+    try:
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            create_table_query = """
+            CREATE TABLE IF NOT EXISTS posts (
+                id serial PRIMARY KEY,
+                title VARCHAR (100),
+                content TEXT,
+                user_id INTEGER REFERENCES users(id)
+            )
+            """
+            cursor.execute(create_table_query)
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return True
+    except psycopg2.Error as e:
+        print("Ошибка при создании таблицы постов:", e)
+        
+# Функция для валидации данных поста перед добавлением
+def validate_post_data(title, content):
+    if not title or not content:
+        return False  # Если хотя бы одно из полей пусто, возвращаем False
+    return True  # Если данные прошли все проверки, возвращаем True
+
+# Функция для добавления поста
+def add_post(title, content, user_id):
+    # Проверяем валидность данных перед добавлением
+    if not validate_post_data(title, content):
+        return False  # Данные не прошли валидацию, не выполняем добавление
+
+    try:
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            insert_query = """
+            INSERT INTO posts (title, content, user_id)
+            VALUES (%s, %s, %s)
+            """
+            cursor.execute(insert_query, (title, content, user_id))
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return True
+    except psycopg2.Error as e:
+        print("Ошибка при добавлении поста:", e)
+        return False  # Ошибка при добавлении
+
+
+def delete_post(post_id):
+    try:
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            delete_query = "DELETE FROM posts WHERE id = %s"
+            cursor.execute(delete_query, (post_id,))
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return True
+    except psycopg2.Error as e:
+        print("Ошибка при удалении поста:", e)
+
+def get_all_posts():
+    try:
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            select_query = "SELECT * FROM posts"
+            cursor.execute(select_query)
+            posts = cursor.fetchall()
+            cursor.close()
+            conn.close()
+            return posts
+    except psycopg2.Error as e:
+        print("Ошибка при получении всех постов:", e)
+
+def get_post_by_id(post_id, fields=("id", "title")):
+    try:
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            select_query = "SELECT {} FROM posts WHERE id = %s".format(", ".join(fields))
+            cursor.execute(select_query, (post_id,))
+            post_data = cursor.fetchone()
+            cursor.close()
+            conn.close()
+            return post_data
+    except psycopg2.Error as e:
+        print("Ошибка при получении поста по ID:", e)
+
+def update_post(post_id, new_title, new_content):
+    # Проверяем валидность данных перед обновлением
+    if not validate_post_data(new_title, new_content):
+        return False  # Данные не прошли валидацию, не выполняем обновление
+
+    try:
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            update_query = """
+            UPDATE posts
+            SET title = %s, content = %s
+            WHERE id = %s
+            """
+            cursor.execute(update_query, (new_title, new_content, post_id))
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return True
+    except psycopg2.Error as e:
+        print("Ошибка при обновлении информации о посте:", e)
+        return False  # Ошибка при обновлении
+        
+# Функция для получения всех постов с сортировкой по дате публикации (по умолчанию по возрастанию)
+def get_all_posts_sort(order_by="created_at ASC"):
+    try:
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            select_query = f"SELECT * FROM posts ORDER BY {order_by}"
+            cursor.execute(select_query)
+            posts = cursor.fetchall()
+            cursor.close()
+            conn.close()
+            return posts
+    except psycopg2.Error as e:
+        print("Ошибка при получении постов:", e)
+        
+def drop_post_table():
+    try:
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            drop_table_query = "DROP TABLE IF EXISTS posts CASCADE"
+            cursor.execute(drop_table_query)
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return True
+    except psycopg2.Error as e:
+        print("Ошибка при удалении таблицы постов:", e)
